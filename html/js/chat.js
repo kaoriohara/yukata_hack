4f31a1cb86c94e85ff64249db7845043ffd4d30e
Sho <daxjps@gmail.com>
2015-08-01 17:21:22 +0900

    jsのメッセージ送信を追加

---
+++ html/js/chat.js
@@ -0,0 +1,47 @@
+$(function(){
+ // LoadMessage();
+  $('#message_sendButton').click(function(){SendMessage();});
+});
+
+function SendMessage(){
+  requestData = {
+    body : $('#message_textBox').val(),
+    position_x : "bbbb",
+    position_y :"cccc"
+  };
+  alert(requestData.body);
+  $.ajax({ 
+    type : 'POST', 
+    url : 'http://localhost:3000/save_post', 
+    data : JSON.stringify(requestData), 
+    contentType: 'application/JSON', 
+    dataType : 'JSON', 
+    scriptCharset: 'utf-8', 
+    success : function(data) { 
+      // Success 
+      alert("success"); 
+    }, 
+    error : function(data) { 
+      // Error 
+      alert("error"); 
+    } 
+  });
+}
+
+function LoadMessage(){
+  postion = {x:"bbbb", y:"cccc"};
+  $.ajax({ 
+    type : 'POST', 
+    url : 'http://localhost:3000/get_post?position_x=' & postion.x & '&position_y=' & postion.y, 
+    success : function(data) { 
+      // Success 
+      alert("success"); 
+      $.each(data,function(){$('#message_area').prepend(this.body & '<br />');});
+    }, 
+    error : function(data) { 
+      // Error 
+      alert("error"); 
+    } 
+  });
+  setTimeout('LoadMessage()',10000);
+}
