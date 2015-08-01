bc11b4eaf3d7968fa7725ce4058ff6bf7b16d7df
yada <ydt.hran2@gmail.com>
2015-08-01 16:42:12 +0900

    取得するイベントの形式に対応

--- map/js/script.js
+++ map/js/script.js
@@ -1,24 +1,5 @@
 /*global $, google */
 window.onload = function() {
-  /*
-  $.ajax('/event/', {
-    method: 'GET',
-    dataType: 'json'
-  }).done(function(locations) {
-    showMap(locations);
-  }).fail(function() {
-    alert('データの取得に失敗しました');
-  });
-  */
-  var locations = [
-    { y: 35.681735, x: 139.762651 },
-    { y: 35.681622, x: 139.766073 },
-    { y: 35.682622, x: 139.766373 },
-  ];
-  showMap(locations);
-};
-
-function showMap(locations) {
   var defaultPos = new google.maps.LatLng(35.681735, 139.762651);
   var icon = new google.maps.MarkerImage('img/ico.png',
     new google.maps.Size(55, 72), // サイズ
@@ -30,8 +11,8 @@ function showMap(locations) {
     mapTypeControlOptions: { mapTypeIds: ['sample', google.maps.MapTypeId.ROADMAP] }/*表示タイプの指定*/
   });
 
-  locations.forEach(function(location) {
-    var pos = new google.maps.LatLng(location.y, location.x);
+  var putMarker = function(x, y) {
+    var pos = new google.maps.LatLng(y, x);
     var title = 'チャットのタイトル';
     var marker = new google.maps.Marker({
       position: pos,
@@ -46,8 +27,27 @@ function showMap(locations) {
     google.maps.event.addListener(marker, 'click', function(e) {
       openChat(e.latLng.G, e.latLng.K);
     });
+  };
+
+  $.ajax('/all_event.json', {
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(res) {
+    res.positions.forEach(function(position) {
+      putMarker(position['position_x'], position['position_y']);
+    });
+  }).fail(function() {
+    alert('データの取得に失敗しました');
+    // TODO: 仮のデータなので消す
+    [
+      { y: 35.681735, x: 139.762651 },
+      { y: 35.681622, x: 139.766073 },
+      { y: 35.682622, x: 139.766373 },
+    ].forEach(function(location) {
+      putMarker(location.x, location.y);
+    });
   });
-}
+};
 
 function openChat(x, y) {
   alert('open chat: ' + x + ', ' + y);
